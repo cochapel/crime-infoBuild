d09d8c12a500b1f65e25ee5135423ebeb6ea8268
Austin Phillips <cochapel@live.unc.edu>
2019-04-27 13:36:45 -0400

    Add initial site files

---
+++ precache-manifest.34c0def54aebca9bddfe938799c512a2.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "32822ace2cacb5d1ffa70116e976409f",
+    "url": "/crime-info/static/media/thief.32822ace.jpg"
+  },
+  {
+    "revision": "5176e26fa7d892a6b8989847cb501f52",
+    "url": "/crime-info/static/media/logo.5176e26f.png"
+  },
+  {
+    "revision": "a213b3c5ccb42a31c042c3bd1bced075",
+    "url": "/crime-info/static/media/goboldBold.a213b3c5.otf"
+  },
+  {
+    "revision": "89a1d495e3ea315d9fa223548a9d9afe",
+    "url": "/crime-info/static/media/Florentia-Regular-trial.89a1d495.ttf"
+  },
+  {
+    "revision": "cc8a8bbcfe4128ee1580",
+    "url": "/crime-info/static/js/runtime~main.8b377e47.js"
+  },
+  {
+    "revision": "7c84cb0a4b0fb1f3619c",
+    "url": "/crime-info/static/js/main.71a1b3d4.chunk.js"
+  },
+  {
+    "revision": "8e706f90367d90f9a04b",
+    "url": "/crime-info/static/js/2.03ab3833.chunk.js"
+  },
+  {
+    "revision": "7c84cb0a4b0fb1f3619c",
+    "url": "/crime-info/static/css/main.66bbbe50.chunk.css"
+  },
+  {
+    "revision": "a624f9bdf1766dc1479474513975d0af",
+    "url": "/crime-info/index.html"
+  }
+];
